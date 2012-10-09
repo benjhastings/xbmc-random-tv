6b5241a8d5c38a1fc12ea6230581808be3194c49
Ben Hastings <benjhastings@gmail.com>
2012-10-09 13:56:31 +0100

    Script is now working correctly.
    Script is working correctly and randomly selecting a show based on the
    provided criteria,

--- script.randomtv/README.txt
+++ script.randomtv/README.txt
@@ -2,3 +2,9 @@ xbmc-random-tv
 ==============
 
 XBMC script for picking random TV Shows from your library
+
+User will be prompted for three things before a show is chosen:
+
+* Unwatched shows only
+* Genre of show (if any)
+* Which show they want to watch (if any)
--- script.randomtv/default.py
+++ script.randomtv/default.py
@@ -2,7 +2,7 @@
 # Massive thanks to the developers of the script.randommitems addon, without whom this would not have been possible
 #
 # Author - el_Paraguayo
-# Website - https://github.com/elParaguayo/
+# Website - https://github.com/benjhastings/xbmc-random-tv/
 # Version - 0.1
 # Compatibility - pre-Eden
 #
@@ -16,154 +16,220 @@ import os
 import random
 import simplejson as json
 
-# let's parse arguments before we start
-try:
-  # parse sys.argv for params
-  params = dict( arg.split( "=" ) for arg in sys.argv[ 1 ].split( "&" ) )
-except:
-  # no params passed
-  params = {}
-# set our preferences
-filterGenres = params.get( "filtergenre", "" ) == "True"
-filterShow = params.get( "filtershow", "" ) == "True"
-promptUser = params.get( "prompt" , "" ) == "True"
+#Set preferences
+filter_genres = False
+filter_shows = False
+prompt_user = True
 
 
-def getMovieLibrary():
-  # get the raw JSON output
-  try:
-    moviestring = unicode(xbmc.executeJSONRPC('{"jsonrpc": "2.0", "method": "VideoLibrary.GetTVShows", "params": { "fields": ["genre", "playcount", "file"]}, "id": 1}'), errors='ignore')
-    movies = json.loads(moviestring)
-    # older "pre-Eden" versions accepted "fields" parameter but this was changed to "properties" in later versions.
-    # the next line will throw an error if we're running newer version
-    testError = movies["result"]
-  except:
-    moviestring = unicode(xbmc.executeJSONRPC('{"jsonrpc": "2.0", "method": "VideoLibrary.GetTVShows", "params": { "properties": ["genre", "playcount", "file"]}, "id": 1}'), errors='ignore')
-    movies = json.loads(moviestring)
-  # and return it
-  return movies
 
-def getRandomMovie(filterWatched, filterGenre, genre):
-  # set up empty list for movies that meet our criteria
-  movieList = []
-  # loop through all movies
-  for movie in moviesJSON["result"]["movies"]:
-    # reset the criteria flag
-    meetsCriteria = False
-    # If we're filtering by genre let's put the genres in a list
-    if filterGenre:
-      moviegenre = []
-      moviegenre = movie["genre"].split(" / ")
-    # check if the film meets the criteria
-    # Test #1: Does the genre match our selection (also check whether the playcount criteria are satisfied)
-    if ( filterGenre and genre in moviegenre ) and (( filterWatched and movie["playcount"] == 0 ) or not filterWatched):
-      meetsCriteria = True
-    # Test #2: Is the playcount 0 for unwatched movies (when not filtering by genre)
-    if ( filterWatched and movie["playcount"] == 0 and not filterGenre ):
-      meetsCriteria = True
-    # Test #3: If we're not filtering genre or unwatched movies, then it's added to the list!!
-    if ( not filterWatched and not filterGenre ):
-      meetsCriteria = True
+def get_tv_show_library():
+    # get the raw JSON output
+    try:
+        shows = unicode(xbmc.executeJSONRPC('{"jsonrpc": "2.0", "method": "VideoLibrary.GetTVShows", "params": { "fields": ["genre", "playcount", "file"]}, "id": 1}'), errors='ignore')
+        shows = json.loads(shows)
+        # older "pre-Eden" versions accepted "fields" parameter but this was changed to "properties" in later versions.
+        # the next line will throw an error if we're running newer version
+        testError = shows["result"]
+    except KeyError:
+        shows = unicode(xbmc.executeJSONRPC('{"jsonrpc": "2.0", "method": "VideoLibrary.GetTVShows", "params": { "properties": ["genre", "playcount", "file"]}, "id": 1}'), errors='ignore')
+        shows = json.loads(shows)
+        # and return it
+    return shows
 
-    # if it does, let's add the file path to our list
-    if meetsCriteria:
-      movieList.append(movie["file"])
-  # Make a random selection      
-  randomMovie = random.choice(movieList)
-  # return the filepath
-  return randomMovie
-  
-def selectGenre(filterWatched):
-  success = False
-  selectedGenre = ""
-  myGenres = []
-  
-  for movie in moviesJSON["result"]["movies"]:
-    # Let's get the movie genres
-    # If we're only looking at unwatched movies then restrict list to those movies
-    if ( filterWatched and movie["playcount"] == 0 ) or not filterWatched:
-      genres = movie["genre"].split(" / ")
-      for genre in genres:
-        # check if the genre is a duplicate
-        if not genre in myGenres:
-          # if not, add it to our list
-          myGenres.append(genre)
-  # sort the list alphabeticallt        
-  mySortedGenres = sorted(myGenres)
-  # prompt user to select genre
-  selectGenre = xbmcgui.Dialog().select("Select genre:", mySortedGenres)
-  # check whether user cancelled selection
-  if not selectGenre == -1:
-    # get the user's chosen genre
-    selectedGenre = mySortedGenres[selectGenre]
-    success = True
-  else:
+def get_random_season(random_show):
+    #Gets a random season from the show that is passed in
+    try:
+        seasons = unicode(xbmc.executeJSONRPC('{"jsonrpc": "2.0", "method": "VideoLibrary.GetSeasons", "params": { "tvshowid": %(tvshowid)i, "fields": ["season"]}, "id": 1}' % {'tvshowid':random_show}), errors='ignore')
+        seasons = json.loads(seasons)
+        testError = seasons["result"]
+    except KeyError:
+        seasons = unicode(xbmc.executeJSONRPC('{"jsonrpc": "2.0", "method": "VideoLibrary.GetSeasons", "params": { "tvshowid": %(tvshowid)i, "properties": ["season"]}, "id": 1}' % {'tvshowid':random_show}), errors='ignore')
+        seasons = json.loads(seasons)
+    season_list = []
+    for season in seasons['result']['seasons']:
+        if season not in season_list:
+            season_list.append(season['season'])
+    random_season = random.choice(season_list)
+    return random_season
+
+def get_random_episode(filter_watched, random_show, random_season):
+    #gets a random episode from the show and the season that are passed in
+    try:
+        episodes = unicode(xbmc.executeJSONRPC('{"jsonrpc": "2.0", "method": "VideoLibrary.GetEpisodes", "params": { "tvshowid": %(tvshowid)i, "season" :%(season_id)i, "fields": ["season", "file", "playcount"]}, "id": 1}' % {'tvshowid':random_show, 'season_id':random_season}), errors='ignore')
+        episodes = json.loads(episodes)
+        testError = episodes['result']
+    except KeyError:
+        episodes = unicode(xbmc.executeJSONRPC('{"jsonrpc": "2.0", "method": "VideoLibrary.GetEpisodes", "params": { "tvshowid": %(tvshowid)i, "season" :%(season_id)i, "properties": ["season", "file", "playcount"]}, "id": 1}' % {'tvshowid':random_show, 'season_id':random_season}), errors='ignore')
+        episodes = json.loads(episodes)
+    episode_list = []
+    for episode in episodes['result']['episodes']:
+        #Checks to make sure the individual episode is unwatched or not (depending on whether we are filtering for unwatched)
+        if episode not in episode_list and check_unwatched(filter_watched, episode):
+            episode_list.append(episode['file'])
+    random_episode = random.choice(episode_list)
+    return random_episode
+
+def check_show(selected_show, show):
+    #Checks to see if the show is valid for selection by seeing if the show matches the selected show
+    return True if selected_show is not None and selected_show == show['label'] else False
+
+def check_genre(selected_genre, show):
+    #Checks to see if the show is valid for selection by seeing if the shows genre matches the selected genre
+    if selected_genre is not None:
+        showsgenre = []
+        showsgenre = show["genre"].split(" / ")
+    return True if selected_genre is not None and selected_genre in showsgenre else False
+
+def check_unwatched(filter_watched, object):
+    #Checks to see if the show is valid by checking to see if the show (or episode) has been watched
+    return True if ( filter_watched and object["playcount"] == 0 ) or not filter_watched else False
+
+def get_random_show(filter_watched, selected_genre, selected_show):
+    #Set up an empty list for the tv shows
+    shows_list = []
+    # loop through all tv shows
+    for show in tv_shows_json["result"]["tvshows"]:
+        # reset the criteria flag
+        meetsCriteria = False
+
+        #Check if the entire show is unwatched or not
+        if check_unwatched(filter_watched, show):
+            #Check to see if the show matches the criteria
+            if check_show(selected_show, show):
+                meetsCriteria = True
+            #Check to see if the genre matches the criteria (if it does also check to make sure the show matches the selected genre)
+            if check_genre(selected_genre, show):
+                meetsCriteria = True
+                if check_show(selected_show, show):
+                    meetsCriteria = True
+                else:
+                    meetsCriteria = False
+            #If we haven't been asked to filter by watched, genre or show then add it to the list
+            if ( not filter_watched and selected_genre is None and selected_show is None ):
+                meetsCriteria = True
+
+            #If it meets the criteria then add it to the list
+            if meetsCriteria:
+                shows_list.append(show["tvshowid"])
+
+    #Picks a random show from the list
+    random_show = random.choice(shows_list)
+    #Picks a random season of this show
+    random_season = get_random_season(random_show)
+    #Picks a random episode of this season
+    random_episode = get_random_episode(filter_watched, random_show, random_season)
+    # return the filepath
+    return random_episode
+
+def select_genre(filter_watched):
+    """
+    Select the genre
+    """
     success = False
-  # return the genre and whether the choice was successfult
-  return success, selectedGenre
-  
-  
-def getUnwatched():
-  # default is to select from all movies
-  unwatched = False
-  # ask user whether they want to restrict selection to unwatched movies
-  a = xbmcgui.Dialog().yesno("Watched movies", "Restrict selection to unwatched movies only?")
-  # deal with the output
-  if a == 1: 
-    # set restriction
-    unwatched = True
-  return unwatched
-  
-def askGenres():
-  # default is to select from all movies
-  selectGenre = False
-  # ask user whether they want to select a genre
-  a = xbmcgui.Dialog().yesno("Select genre", "Do you want to select a genre to watch?")
-  # deal with the output
-  if a == 1: 
-    # set filter
-    selectGenre = True
-  return selectGenre
+    my_genres = []
+    selected_genre = None
+    for show in tv_shows_json["result"]["tvshows"]:
+        # Let's get the tv show genres
+        # If we're only looking at unwatched movies then restrict list to those movies
+        if check_unwatched(filter_watched, show):
+            genres = show["genre"].split(" / ")
+            for genre in genres:
+                # check if the genre is a duplicate
+                if not genre in my_genres:
+                    # if not, add it to our list
+                    my_genres.append(genre)
+    #sort the list
+    my_genres = sorted(my_genres)
+    #prompt user to select genre
+    select_genre_dialog = xbmcgui.Dialog().select("Select genre:", my_genres)
+    #check whether user cancelled selection
+    if not select_genre_dialog == -1:
+        # get the user's chosen genre
+        selected_genre = my_genres[select_genre_dialog]
+        success = True
+    else:
+        success = False
+    # return the genre and whether the choice was successfult
+    return success, selected_genre
 
-def askTVShow():
-    # default is to select from all movies
-    selectShow = False
-    # ask user whether they want to select a genre
-    a = xbmcgui.Dialog().yesno("Select Show", "Do you want to select a spsecific show to watch?")
+def select_show(filter_watched, selected_genre):
+    """
+    Select the show
+    """
+    success = False
+    my_shows = []
+    selected_show = None
+    for show in tv_shows_json["result"]["tvshows"]:
+        if check_unwatched(filter_watched, show):
+            #If a genre has been specified then check to see if it matches the show
+            #If it does add it to the list.
+            #If no genre specified then add the show to the list
+            if selected_genre is not None:
+                genres = show["genre"].split(" / ")
+                for genre in genres:
+                    # check if the genre is a duplicate
+                    if selected_genre in show['genre'] and show['label'] not in my_shows:
+                        # if not, add it to our list
+                        my_shows.append(show['label'])
+            else:
+                my_shows.append(show['label'])
+    #Sort the list
+    my_shows = sorted(my_shows)
+    #prompt user to select show
+    select_show_dialog = xbmcgui.Dialog().select("Select show:", my_shows)
+    #check whether user cancelled selection
+    if not select_show_dialog == -1:
+        # get the user's chosen genre
+        selected_show = my_shows[select_show_dialog]
+        success = True
+    else:
+        success = False
+    #return the genre and whether the choice was successfult
+    return success, selected_show
+
+def ask_question(type=False, unwatched=False):
+    #Asks user a question
+    select_answer = False
+
+    if type == False and unwatched == True:
+        a = xbmcgui.Dialog().yesno("Watched Episodes" % {'type':type}, "Restrict selection to unwatched episodes only?")
+    else:
+        a = xbmcgui.Dialog().yesno("Select %(type)s" % {'type':type}, "Do you want to select a %(type)s to watch?" % {'type':type})
     # deal with the output
     if a == 1:
     # set filter
-        selectShow = True
-    return selectShow
+        select_answer = True
+    return select_answer
 
 
-# get the full list of movies from the user's library
-moviesJSON = getMovieLibrary()
-  
-# ask user if they want to only play unwatched movies  
-unwatched = getUnwatched()  
+#Get the full list of shows from a users library
+tv_shows_json = get_tv_show_library()
 
-# is skin configured to use one entry?
-if promptUser and not filterGenres:
-  # if so, we need to ask whether they want to select genre
-  filterGenres = askGenres()
+#Ask user if they want to restrict the episodes to unwatched only
+unwatched = ask_question(unwatched=True)
 
-# did user ask to select genre?
-success, selectedGenre = selectGenre(unwatched)
-randomMovie = getRandomMovie(unwatched, success, selectedGenre)
-xbmc.executebuiltin('PlayMedia(' + randomMovie + ',0,noresume)')
+#We want to prompt the user
+if prompt_user:
+    #We haven't prompted them for a genre yet
+    if not filter_genres:
+        filter_genres = ask_question('genre')
+        selected_genre = None
+        genre_success = True
+        if filter_genres:
+            genre_success, selected_genre = select_genre(unwatched)
 
-#  # bring up genre dialog
-#  success, selectedGenre = selectGenre(unwatched)
-#  # if not aborted
-#  if success:
-#    # get the random movie...
-#    randomMovie = getRandomMovie(unwatched, True, selectedGenre)
-#    # ...and play it!
-#    xbmc.executebuiltin('PlayMedia(' + randomMovie + ',0,noresume)')
-#else:
-#  # no genre filter
-#  # get the random movie...
-#  randomMovie = getRandomMovie(unwatched, False, "")
-#  # ...and play it
-#  xbmc.executebuiltin('PlayMedia(' + randomMovie + ',0,noresume)')
+    #We haven't prompted them for a show yet
+    if not filter_shows:
+        filter_shows = ask_question('show')
+        selected_show = None
+        show_success = True
+        if filter_shows:
+            show_success, selected_show = select_show(unwatched, selected_genre)
+
+#If both genre and show returned successfully (i.e. the user didn't exit out from the choice screen)
+#then get a random episode that matches the criteria
+if genre_success and show_success:
+    random_episode = get_random_show(unwatched, selected_genre, selected_show)
+    if random_episode:
+        xbmc.executebuiltin('PlayMedia(' + random_episode + ',0,noresume)')
